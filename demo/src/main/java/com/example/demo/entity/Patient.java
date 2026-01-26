e46d9aa7dc959c74be427f720125697881c97694
LOVESH1808 <loveshdutta1808@gmail.com>
2026-01-26 22:00:35 +0530

    first commit

---
+++ demo/src/main/java/com/example/demo/entity/Patient.java
@@ -0,0 +1,18 @@
+package com.example.demo.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Patient {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long ID;
+
+    private String name;
+
+    private String email;
+}
