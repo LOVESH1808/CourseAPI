b908daf55da4afbee7dbb409afd7eac29f1c83e2
LOVESH1808 <loveshdutta1808@gmail.com>
2026-02-02 17:30:40 +0530

    deployment changes 8

--- demo/src/main/java/com/example/demo/config/OpenApiConfig.java
+++ demo/src/main/java/com/example/demo/config/OpenApiConfig.java
@@ -4,6 +4,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -15,18 +16,29 @@ public class OpenApiConfig {
         @Bean
         public OpenAPI openAPI() {
                 return new OpenAPI()
-                        .info(new Info()
-                                .title("Course Progress API")
-                                .version("1.0"))
-                        .addSecurityItem(new SecurityRequirement()
-                                .addList(SECURITY_SCHEME_NAME))
-                        .components(new io.swagger.v3.oas.models.Components()
-                                .addSecuritySchemes(SECURITY_SCHEME_NAME,
-                                        new SecurityScheme()
-                                                .name(SECURITY_SCHEME_NAME)
-                                                .type(SecurityScheme.Type.HTTP)
-                                                .scheme("bearer")
-                                                .bearerFormat("JWT")
-                                ));
+                        .addServersItem(
+                                new Server()
+                                        .url("https://courseapi-production-b1da.up.railway.app")
+                        )
+                        .info(
+                                new Info()
+                                        .title("Course Progress API")
+                                        .version("1.0")
+                        )
+                        .addSecurityItem(
+                                new SecurityRequirement()
+                                        .addList(SECURITY_SCHEME_NAME)
+                        )
+                        .components(
+                                new io.swagger.v3.oas.models.Components()
+                                        .addSecuritySchemes(
+                                                SECURITY_SCHEME_NAME,
+                                                new SecurityScheme()
+                                                        .name(SECURITY_SCHEME_NAME)
+                                                        .type(SecurityScheme.Type.HTTP)
+                                                        .scheme("bearer")
+                                                        .bearerFormat("JWT")
+                                        )
+                        );
         }
 }
